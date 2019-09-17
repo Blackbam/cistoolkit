7e2fc0f399bd874358789cf1927195bfb8c444ca
David Stöckl <david.stoeckl@blackbam.at>
2019-09-17 11:23:38 +0200

    feat. a more useful basic enum function

--- src/Enum/BasicEnum.php
+++ src/Enum/BasicEnum.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types = 1);
+
 namespace CisTools\Enum;
 
 /**
@@ -16,7 +18,11 @@ abstract class BasicEnum {
     private function __construct() {
     }
 
-    private static function getConstants() {
+    /**
+     * @return array: Associative array of class constants (key and value).
+     * @throws \ReflectionException
+     */
+    public static function getConstants(): array {
         if (self::$constCacheArray == NULL) {
             self::$constCacheArray = [];
         }
@@ -28,7 +34,13 @@ abstract class BasicEnum {
         return self::$constCacheArray[$calledClass];
     }
 
-    public static function isValidName($name, $strict = false) {
+    /**
+     * @param string $name: Class constant name
+     * @param bool $strict: Take casing into account?
+     * @return bool
+     * @throws \ReflectionException
+     */
+    public static function isValidName(string $name, bool $strict = false): bool {
         $constants = self::getConstants();
 
         if ($strict) {
@@ -39,8 +51,48 @@ abstract class BasicEnum {
         return in_array(strtolower($name), $keys);
     }
 
-    public static function isValidValue($value, $strict = true) {
+    /**
+     * @param $value: Value to check
+     * @param bool $strict
+     * @return bool
+     * @throws \ReflectionException
+     */
+    public static function isValidValue($value, bool $strict = true): bool {
         $values = array_values(self::getConstants());
         return in_array($value, $values, $strict);
+    }
+
+    /**
+     * @return array: All defined constant names
+     * @throws \ReflectionException
+     */
+    public static function getNames(): array {
+        return array_keys(self::getConstants());
+    }
+
+    /**
+     * @param string $key: The  name of the class constant
+     * @param bool $strict: Take casing into account
+     * @return mixed|null: The constants actual value, but NULL if it does not exist
+     * @throws \ReflectionException
+     */
+    public static function getValue(string $key, bool $strict = false) {
+        $constants = self::getConstants();
+
+        foreach($constants as $cname => $cvalue) {
+            if(!$strict) {
+                if(strtolower($key) === strtolower($cname)) {
+                    return $cvalue;
+                }
+            } else {
+                if($key === $cname) {
+                    return $cvalue;
+                }
+            }
+        }
+        if(array_key_exists($key,$constants)) {
+            return $constants[$key];
+        }
+        return null;
     }
 }
