bc75e00858ce0e311dea2f34079aa81a0d1131e0
David Stöckl <david.stoeckl@blackbam.at>
2021-02-26 16:23:14 +0100

    fix. improved Html attribute checker (better pattern, irritating isValid returining exact opposite

--- src/Html.php
+++ src/Html.php
@@ -6,16 +6,16 @@ use CisTools\Exception\NonSanitizeableException;
 
 class Html
 {
-    public const ATTRIBUTE_NAME_MATCHER = "/[\s\x{0000}\x{0020}\x{0022}\x{0027}\x{003E}\x{002F}\x{003D}\x{200B}-\x{200D}\x{FDD0}-\x{FDEF}\x{FEFF}[:cntrl:]]+/u";
+    public const ATTRIBUTE_NAME_MATCHER = "/[\s\p{Cc}\x{0000}\x{0022}\x{0027}\x{003E}\x{002F}\x{003D}\x{200B}-\x{200D}\x{FDD0}-\x{FDEF}\x{FEFF}]+/u";
 
     /**
-     * Checks if a given string is a valid HTML attribute.
+     * Checks if a given string is a valid HTML attribute name.
      * @param string $attributeName
      * @return bool: True if the given attribute name is a valid HTML attribute name.
      */
     public static function isAttributeNameValid(string $attributeName): bool
     {
-        return (bool)preg_match(self::ATTRIBUTE_NAME_MATCHER, $attributeName);
+        return !preg_match(self::ATTRIBUTE_NAME_MATCHER, $attributeName);
     }
 
     /**
@@ -28,7 +28,7 @@ class Html
     {
         $sanitizedAttributeName = preg_replace(self::ATTRIBUTE_NAME_MATCHER, '', $attributeName);
         if(!$sanitizedAttributeName) {
-            throw new NonSanitizeableException("Failed to sanitize attribute name");
+            throw new NonSanitizeableException("Attribute name had no salvageable characters");
         }
         return $sanitizedAttributeName;
     }
