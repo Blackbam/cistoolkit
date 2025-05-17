d3ee4fff06edb3fe71afc39d676575cde2dc596a
David Stöckl <david.stoeckl@blackbam.at>
2025-05-17 14:41:27 +0200

    feat. update CSRF Protector

--- src/CSRFProtector.php
+++ src/CSRFProtector.php
@@ -6,23 +6,23 @@ use CisTools\Exception\InvalidArgumentException;
 
 /**
  * Implements simple double-submit cookie pattern protection.
- *
- * Use like: if (!CSRFProtector::isValidToken((string) $cpt)) - $cpt must be within a header (or, less good, in a form field).
  */
 class CSRFProtector
 {
 
     public const COOKIE_NAME = "_cpt";
+    private static ?string $currentCookie = null;
 
     /**
      * @return void
-     * @throws InvalidArgumentException: If there is an issue with generating the random string, not very likely to happen.
+     * @throws InvalidArgumentException : If there is an issue with generating the random string, not very likely to happen.
      */
     public static function setCsrfCookie(): void
     {
+        $randomString = StringGenerator::generateSecureRandomString(40, StringGenerator::LOWERCASE | StringGenerator::UPPERCASE | StringGenerator::NUMBERS);
         setcookie(
             self::COOKIE_NAME,
-            StringGenerator::generateSecureRandomString(40, StringGenerator::LOWERCASE | StringGenerator::UPPERCASE | StringGenerator::NUMBERS),
+            $randomString,
             [
                 'expires' => 0,
                 'path' => '/',
@@ -32,9 +32,35 @@ class CSRFProtector
                 'samesite' => 'Strict',
             ]
         );
-
+        self::$currentCookie = $randomString;
     }
 
+    /**
+     * @return string|null
+     */
+    public static function getCurrentCsrfCookie(): ?string
+    {
+        return self::$currentCookie;
+    }
+
+    /**
+     * @return string
+     * @throws InvalidArgumentException
+     */
+    public static function getOrSetCurrentCsrfCookie(): string
+    {
+        if(is_null(self::$currentCookie)) {
+            self::setCsrfCookie();
+        }
+        return self::$currentCookie;
+    }
+
+    /**
+     * Compare this in the answer request.
+     *
+     * @param string $token
+     * @return bool
+     */
     public static function isValidToken(string $token): bool
     {
         return $token === $_COOKIE[self::COOKIE_NAME];
